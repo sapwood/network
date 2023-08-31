8c12d2e636482bcc0c915d9441b5b55f8d603f35
Brian <eyenani@gmail.com>
2023-08-30 19:03:41 -0600

    scroll down to the result

--- static/js.js
+++ static/js.js
@@ -12,6 +12,10 @@ $(document).ready(function(){
                 $('#result').html(response);
             }
         });
+
+        $('html, body').animate({
+            scrollTop: $("#result").offset().top
+         }, 1000);
     });
 
     
